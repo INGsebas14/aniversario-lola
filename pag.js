a6cc5cb6e432774662b0898c6ff025263619e663
Sebastian Fonseca <safonsecam@libertadores.edu.co>
2024-09-17 18:14:32 -0500

    cargando todos los archivos

---
+++ pag.js
@@ -0,0 +1,12 @@
+function mensaje() {
+    window.location.href="https://www.instagram.com/reel/C61aH6Zrg6-/?utm_source=ig_web_copy_link";
+}
+function modal() {
+    document.getElementById("modal").style.display = "none";
+    document.getElementById("laCarta").style.display = "block";
+}
+function cerrarModal() {
+    document.getElementById("laCarta").style.display = "none";
+    document.getElementById("modal").style.display = "block";
+}
+
